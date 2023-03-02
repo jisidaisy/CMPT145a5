e766c0c59ac40b9b3ee83867f2d660a97f755a7d
jisidaisy <jisidaisy@yahoo.com>
2023-03-01 19:26:10 -0600

    Added Docstrings
    Added some interface documentation to represent the function shorthands to be used in the program

---
+++ a5q4.py
@@ -0,0 +1,7 @@
+"""
+Assuming node class contains the below methods to set and get the data and next node values.
+get_data() returns the value of the node
+set_data(value) updates the value of the node
+get_next() returns the next node in the chain (None if last node)
+set_next(n) sets the next node of the node to n
+"""
