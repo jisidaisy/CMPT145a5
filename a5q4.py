517de381a5b3de47b13a56b1939efa9dc7da2ca5
jisidaisy <jisidaisy@yahoo.com>
2023-03-01 19:29:53 -0600

    Created Function sumnc
    This function return the sum of the data values in the node chain

--- a5q4.py
+++ a5q4.py
@@ -31,3 +31,26 @@ def sumnc(node_chain):
 
     return total
 
+def count_in(node_chain, value):
+    """
+    Purpose:
+        Counts the number of times a value appears in a node chain
+    Pre-conditions:
+        : param node_chain: a node-chain, possibly empty
+        : param value: a data value
+    Return:
+        :return: The number of times the value appears in the node chain
+    """
+    # initialize count to 0
+    count = 0
+    curr = node_chain  # set curr to first node of the chain
+
+    # loop over the chain
+    while curr is not None:
+        if curr.get_data() == value:  # curr's value is value, increment count by 1
+            count += 1
+
+        curr = curr.get_next()  # move curr to next node in chain
+
+    return count
+
