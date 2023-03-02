cbbc5f8603b71bf7ae1d9232cdfafe13195af30b
jisidaisy <jisidaisy@yahoo.com>
2023-03-01 19:11:04 -0600

    Opened Monsters Text File and generated the desired Output
    OPened text file in read mode and used conditional statements to create battle rules and strategies and added an exception handling block when file is not found or accessible

--- a5q2.py
+++ a5q2.py
@@ -15,3 +15,45 @@ def main():
     try:
         # Create queue ADT to store space monsters
         battle = Queue()
+
+        # Open the file in read mode
+        with open(fileName, 'r') as f:
+            for line in f.readlines():
+                # remove any empty spaces from the line
+                item = line.rstrip('\n').strip()
+                #  if the current item is Godzilla
+                if item == GODZILLA:
+                    # Verify if any space monsters are waiting in the queue
+                    if battle.is_empty() == False:
+                        # Godzilla defeats the space monster that is currently first in the queue
+                        godzillaDefeated.append(battle.dequeue())
+                elif item == MOTHRA:
+                    # Verify if any space monsters are waiting in the queue
+                    if battle.is_empty() == False:
+                        # Mothra defeats the space monster that is currently first in the queue
+                        mothraDefeated.append(battle.dequeue())
+                else:
+                    # it is space monster, so enqueue the item
+                    battle.enqueue(item)
+        # file read is success and battle is completed
+
+        # if there is space monster left in the queue
+        if battle.is_empty() == False:
+            print("Oh no! The space monsters won thanks to " + str(battle.dequeue()))
+        else:
+            # Earth is saved by Godzilla and Mothra
+            print("The space monsters were beaten!\n")
+            # Print the space monsters defeated by Godzilla and Mothra
+            print("Godzilla defeated:")
+            for monster in godzillaDefeated:
+                print(monster)
+
+            print("\nMothra defeated:")
+            for monster in mothraDefeated:
+                print(monster)
+    except IOError:
+        print("File ", fileName, " not accessible")
+
+
+if __name__ == '__main__':
+    main()
